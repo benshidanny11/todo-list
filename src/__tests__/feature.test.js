d1ecb2e27f6c037ceed346bad6d969d104545740
Gambit142 <nzefrancismaryeugorji@gmail.com>
2021-10-01 09:44:24 +0100

    Test that the dit tasks function, update status, and delete all functions work properly

--- src/__tests__/feature.test.js
+++ src/__tests__/feature.test.js
@@ -1,10 +1,11 @@
 /*
  * @jest-environment jsdom
  */
-/* eslint-disable import/no-extraneous-dependencies */
 
-import { describe, expect, test } from '@jest/globals';
-import { addTodo, removeToDo } from '../__mocks__/features.js';
+import {
+  addTodo, removeToDo, editToDo,
+  updateStatus, deleteCompleted,
+} from '../__mocks__/features.js';
 import { localStorage } from '../__mocks__/localStorage.js';
 
 describe('Test to do list add and remove features', () => {
@@ -18,8 +19,23 @@ describe('Test to do list add and remove features', () => {
     expect(localStorage.getItem(1).data.descrption).toBe('Go to gym');
   });
   test('Should remove todo item from local storage ', () => {
-    removeToDo(1);
+    removeToDo(2);
     expect(localStorage.items.length).toBe(1);
-    expect(localStorage.getItem(1).completed).toBeFalsy();
+    expect(localStorage.getItem(1).data.completed).toBe(false);
+  });
+  test('Should Edit The Value Inputted', () => {
+    document.body.innerHTML = '<input type="text" value="Go to gym" id="todo-input">';
+    editToDo('Take A Walk', 1);
+    const taskEdited = document.getElementById('todo-input').value;
+    expect(localStorage.getItem(1).data.descrption).toBe('Take A Walk');
+    expect(taskEdited).toBe('Take A Walk');
+  });
+  test('Should Update Status of Task', () => {
+    updateStatus(1);
+    expect(localStorage.getItem(1).data.completed).toBe(true);
+  });
+  test('Should remove all completed taks', () => {
+    deleteCompleted();
+    expect(localStorage.items.length).toBe(0);
   });
 });
